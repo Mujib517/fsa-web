27f8403639dc7ebb67788b36bb3481c2bdab706c
md.rahman <md.rahman@mdrahmans-MBP.Dlink>
2021-12-29 12:09:32 +0530

    js vars and data types

---
+++ index.js
@@ -0,0 +1,35 @@
+// output statement
+// node : JS runtime
+// JRE : java run time
+// execute js
+// variables
+// why how
+// 8LPA
+// data : variables
+// age: +number
+// rollno: 12A20
+// operations: methods
+// Loosely typed lang
+// strongly typed lang
+// camelcasing:
+// pascalcasing 
+var x = 10.20;
+// number
+// string
+// boolean
+// object
+// function
+// typof opeartor
+// scalar
+var a = "abc";
+a = "xyz";
+var active = true; // 0 or 1
+console.log(typeof a);
+console.log(typeof active);
+console.log(typeof x);
+
+
+
+
+
+
---
+++ index2.js
@@ -0,0 +1,33 @@
+// 0 1
+// instructions per second
+// 1 Ghz = 10^9 instructions per second
+// 100kb
+// Tradeoff: memory & speed
+// hardware -> OS -> C -> C#, Java -> Program 
+var a = 10; // 64 bit 0000 0000 000 ... 1010
+var b = 20;
+var x = 'a'; //0000 000 0000 0000 ... 1100 
+var active = true;  // 0 or 1
+                    // blocks
+                    // 1byte = 8bits
+                    // 0000 0001 8bits true
+                    // 0000 0000 8 bit false
+                    // 1 bit os: 0,1
+                    // 2 bit os: 00 01 10 11
+                    // OS 32 bit: 2^32 = 4GB
+                    // 64 bit: 2^64
+var c = a + b;
+
+console.log(c);
+
+// index.java -> .jar -> output
+// int x = "abc";
+// x/y: DivideByzero
+// compiled lang
+// index.js -> output
+// Disk -> loaded into RAM -> compilation 
+// -> runtime errors
+// 0x122 -> 10
+// 0x123 -> 20
+// 0x223 -> 30
+// 
